ce56ee7e83617707c15a79160044613de74e4b1a
QYChenMin <2019131420@qq.com>
2018-11-29 09:33:18 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgt/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgt.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:ChenMing
+ * creatTime:2018-11-29 09:07
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交 啦啦啦");
+    }
+}
